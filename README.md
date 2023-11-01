4dbacb19d56438a0b02963a779cfcdde29664c99
Stefan Sauer <st.kost@gmail.com>
2023-11-01 19:34:32 +0100

    Initial version of time and frequency calculator.
    Start from the bitwig extension template. Implement the logic to listen
    to tempo changes and show the time and frequency values for different
    note length.

---
+++ README.md
@@ -0,0 +1,10 @@
+# TimeCalc
+
+A simple time & frequency calculator for Bitwig Studio.
+
+## Build & install from code
+
+```shell
+mvn install
+cp target/TimeCalc.bwextension ~/Bitwig\ Studio/Extensions/
+```
---
+++ src/main/java/de/sonicpulse/TimeCalcExtensionDefinition.java
@@ -0,0 +1,81 @@
+package de.sonicpulse;
+import java.util.UUID;
+
+import com.bitwig.extension.api.PlatformType;
+import com.bitwig.extension.controller.AutoDetectionMidiPortNamesList;
+import com.bitwig.extension.controller.ControllerExtensionDefinition;
+import com.bitwig.extension.controller.api.ControllerHost;
+
+public class TimeCalcExtensionDefinition extends ControllerExtensionDefinition
+{
+   private static final UUID DRIVER_ID = UUID.fromString("7b2deceb-a3d6-40be-a191-72ff727988f0");
+   
+   public TimeCalcExtensionDefinition()
+   {
+   }
+
+   @Override
+   public String getName()
+   {
+      return "TimeCalc";
+   }
+   
+   @Override
+   public String getAuthor()
+   {
+      return "ensonic";
+   }
+
+   @Override
+   public String getVersion()
+   {
+      return "0.1";
+   }
+
+   @Override
+   public UUID getId()
+   {
+      return DRIVER_ID;
+   }
+   
+   @Override
+   public String getHardwareVendor()
+   {
+      return "ensonic";
+   }
+   
+   @Override
+   public String getHardwareModel()
+   {
+      return "TimeCalc";
+   }
+
+   @Override
+   public int getRequiredAPIVersion()
+   {
+      return 18;
+   }
+
+   @Override
+   public int getNumMidiInPorts()
+   {
+      return 0;
+   }
+
+   @Override
+   public int getNumMidiOutPorts()
+   {
+      return 0;
+   }
+
+   @Override
+   public void listAutoDetectionMidiPortNames(final AutoDetectionMidiPortNamesList list, final PlatformType platformType)
+   {
+   }
+
+   @Override
+   public TimeCalcExtension createInstance(final ControllerHost host)
+   {
+      return new TimeCalcExtension(this, host);
+   }
+}
